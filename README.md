14676104189699ba94278649da44fa90232073f5
dominictootell <dominic.tootell@gmail.com>
2014-01-13 20:27:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Real simple example of 2 threads:
+
+One calls put(K,V)
+The other calls remove(K,V).
+
+The remove(K,V), just adds up the size of the returned set into a counter variable.
+
+The threads loop over a int value range, hitting the same key in the map.
+This means the map can contain for the K a set of Int values.
+
+The overall number of items in all the sets returned by .remove, should be the total number of
+values set in the put(K,V).
+
+If we do get the same number of values back, some values have gone missing.
+
+- MainAkkaCmm uses akka.util.ConcurrentMultiMap  ( which exhibits the race condition).
+- MainSynchronizedRemoveAndAdd uses a hacked akka.util.ConcurrentMultiMap (with a big fat synchronized on the remove(K), add(K,V) methods)
---
+++ src/main/scala/org/greencheek/akka/cmm/MainSynchronizedRemoveAndAdd.scala
@@ -0,0 +1,66 @@
+package org.greencheek.akka.cmm
+
+import akka.util.{ConcurrentMultiMap2}
+import java.util.concurrent.{CountDownLatch, ExecutorService, Executors}
+import java.util.Comparator
+import scala.None
+
+/**
+ * Created by dominictootell on 13/01/2014.
+ */
+object MainSynchronizedRemoveAndAdd {
+  def main(args: Array[String]) {
+    val loops = 1000
+    val cmm = new ConcurrentMultiMap2[String, Int](1,
+      new Comparator[Int] {
+        def compare(a: Int, b: Int): Int = a compareTo b
+      })
+
+    @volatile var count = 0
+    val countDown = new CountDownLatch(2);
+
+    class Producer(map: ConcurrentMultiMap2[String,Int]) extends Runnable {
+
+      def run() {
+        for (i <- 1 to loops) {
+          map.put("1", i)
+        }
+        countDown.countDown();
+      }
+    }
+
+    class Consumer(map: ConcurrentMultiMap2[String,Int]) extends Runnable {
+
+      def run() {
+        for (i <- 1 to loops) {
+          val x = map.remove("1")
+          x match {
+            case Some(y) => count+=y.size
+            case None =>
+
+          }
+        }
+        countDown.countDown();
+
+      }
+    }
+
+
+    val pool: ExecutorService = Executors.newFixedThreadPool(2)
+
+    pool.execute(new Consumer(cmm))
+    pool.execute(new Producer(cmm))
+
+    countDown.await()
+
+    count += cmm.remove("1").get.size
+
+    println(count)
+
+
+
+
+
+    pool.shutdown()
+  }
+}
